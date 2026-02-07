96ba8754fbe3c01328e46e2457900d7dfae40b13
agustinrios80 <agustinr284@gmail.com>
2026-02-07 07:53:33 -0300

    cambio 4

--- server.js
+++ server.js
@@ -268,7 +268,7 @@ app.get("/gallery", async (req, res) => {
         width: 100%;
         height: 55vh;
         min-height: 320px;
-        background: url("/assets/portada.jpg") center / cover no-repeat;
+        background: url("/assets/portada.jpeg") center / cover no-repeat;
         background-size: cover;
         background-position: center;
         border-radius: 0 0 22px 22px;
